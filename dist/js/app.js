4d1600cd8cfa06de328e3aa8f34879f1e59b6efe
azarel fuentes <azarelfuentes5@gmail.com>
2025-06-27 18:03:55 -0600

    se agregan los modales de javascrip

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,7 +13,39 @@ function crearGaleria()
         imagen.alt = 'Galeria Imagen'
 
         //event handler
-        imagen.onclick = function
+        imagen.onclick = function()
+        {
+            mostrarImagen(i)
+        }
         galeria.appendChild(imagen)
     }
+}
+function mostrarImagen(i)
+{
+        const imagen =document.createElement('IMG')
+        imagen.src =  `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Galeria Imagen'
+
+    //generar modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+    modal.appendChild(imagen)
+    
+    //agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+}
+function cerrarModal()
+{
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    setTimeout(()=>
+    {
+        modal?.remove()
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+
+    },500);
 }
